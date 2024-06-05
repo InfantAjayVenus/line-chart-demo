861feba501f12c654574b5e40bd33e25d18de7ed
Infant Ajay Venus <infantcsajay@outlook.com>
2024-06-06 00:15:48 +0530

    [chart] feat: implement zoom and pan

--- src/activeMonitoring/views/Chart.tsx
+++ src/activeMonitoring/views/Chart.tsx
@@ -27,11 +27,40 @@ export function Chart({
         bottom
     }
 }: ChartProps) {
-    const svgRef = useRef<SVGSVGElement>(null);
+    const svgRef = useRef<HTMLDivElement>(null);
     const tooltipRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
+        const zoom = d3
+            .zoom()
+            .scaleExtent([1, 10])
+            .on('zoom', ({ transform }) => {
+                x
+                    .domain(transform.rescaleX(x2).domain())
+                    .range([left, dimensions.width - right].map(d => transform.applyX(d)));
+
+                chartLine.selectChildren().remove();
+                chartLine
+                    .attr('d', line);
+                
+                plotPoints
+                    .attr('cx', (d) => x(d.timestamp))
+
+                xAxis.call(
+                    d3
+                        .axisBottom<Date>(x)
+                        .tickFormat(d3.timeFormat('%H:%M:%S'))
+                        .ticks(transform.k * 7)
+                ).attr('transform', `translate(${0}, ${dimensions.height - bottom})`)
+            })
+
         const x = d3.scaleUtc(
+            d3.extent(data.map(item => item.timestamp)).reverse() as Iterable<Date>,
+            [left, dimensions.width - right],
+
+        );
+
+        const x2 = d3.scaleUtc(
             d3.extent(data.map(item => item.timestamp)).reverse() as Iterable<Date>,
             [left, dimensions.width - right],
 
@@ -39,7 +68,15 @@ export function Chart({
         const y = d3.scaleLinear(d3.extent(data.map((item) => item.value)) as Iterable<d3.NumberValue>, [dimensions.height - bottom, top]);
         const line = d3.line((d: DataRecordType) => x(d.timestamp), (d: DataRecordType) => y(d.value));
 
-        const svg = d3.select(svgRef.current);
+        const svg = d3
+            .select(svgRef.current)
+            .append('svg')
+            .attr('width', dimensions.width - (left + right))
+            .attr('height', dimensions.height)
+            .call(zoom as any)
+            .append('g')
+            // .attr('transform', `translate(${left}, ${top})`)
+
         const tooltip = d3.select(tooltipRef.current);
         const tooltipRect = tooltipRef.current!.getBoundingClientRect();
         const tooltipXOffset = parseInt((tooltipRect.width / 2).toString());
@@ -63,32 +100,31 @@ export function Chart({
 
 
         // x-axis
-        svg
+        const xAxis = svg
             .append('g')
-            .attr("transform", `translate(0,${dimensions.height - bottom})`)
-            .call(
-                d3.axisBottom<Date>(x)
-                    .tickFormat(d3.timeFormat('%H:%M:%S'))
-            );
+            .call(d3.axisBottom<Date>(x).tickFormat(d3.timeFormat('%H:%M:%S')))
+            .attr("clip-path", "url(#clip)") // add the clip path!
+            .attr('transform', `translate(0, ${dimensions.height - bottom})`)
 
         // Chart line
-        svg
+        const chartLine = svg
             .append('path')
-            .attr("fill", "none")
-            .attr("stroke", "steelblue")
-            .attr('stroke-width', 1.5)
+            .datum(data)
+            .attr('class', 'line')
+            .attr('clip-path', 'url(#clip)')
             .attr('d', line(data));
 
         // Chart Circles
-        svg
+        const plotPoints = svg
             .append('g')
+            .attr('clip-path', 'url(#clip)')
             .selectAll('circle')
             .data(data)
             .join('circle')
+            .attr('class', 'dot')
             .attr('r', 5)
             .attr('cx', (d: DataRecordType) => x(d.timestamp))
             .attr('cy', (d: DataRecordType) => y(d.value))
-            .attr('fill', 'steelblue')
             .on('pointerover', (event) => {
                 const [xPos, yPos] = d3.pointer(event);
                 tooltip
@@ -96,9 +132,9 @@ export function Chart({
                     .html(
                         `
                             <p>
-                                ${d3.timeFormat('%d/%m/%Y %H:%M:%S')(new Date())}
+                                ${d3.timeFormat('%d/%m/%Y %H:%M:%S')(x.invert(xPos))}
                             </p>
-                            <strong>110</strong>
+                            <strong>${Math.ceil(y.invert(yPos))}</strong>
                         `
                     )
                     .style('left', `${xPos + tooltipXOffset}px`)
@@ -109,11 +145,19 @@ export function Chart({
                     .style('opacity', 0);
             })
 
+
+        svg.append("defs").append("clipPath")
+            .attr("id", "clip")
+            .append("rect")
+            .attr("x", 0)
+            .attr("width", dimensions.width)
+            .attr("height", dimensions.height)
+            .attr('x', left)
     }, []);
 
     return (
         <>
-            <svg ref={svgRef} {...dimensions} />
+            <div ref={svgRef} />
             <div className="tooltip" ref={tooltipRef} />
         </>
     );
